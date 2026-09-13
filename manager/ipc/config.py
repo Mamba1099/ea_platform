c546d812d530503469c1cf6d9bd9c0ae736872d4
Mamba1099 <mambakaranja240@gmail.com>
2026-09-14 01:34:30 +0300

    feat(ipc): add configurations settings

---
+++ manager/ipc/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+MT5_COMMON_FILES = Path(
+    "/home/mamba/.wine/drive_c/users/mamba/"
+    "AppData/Roaming/MetaQuotes/Terminal/Common/Files"
+)
+
+IPC_ROOT = MT5_COMMON_FILES / "EAPlatform"
